7e0d86b75d905c0f4b846c213a22c2633182a0bd
bin4xin <1904829268@qq.com>
2021-10-04 23:22:16 +0800

    [2021/10/04/23:22:16] B4xinSync: commit by bin4xin

--- weather.sh
+++ weather.sh
@@ -2,16 +2,16 @@
 
 set -eux
 
-CITY=Shanghai
-LANGUAGE="zh-CN"
-UNIT=m
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+#CITY=Shanghai
+#LANGUAGE="zh-CN"
+#UNIT=m
+#UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl \
-  -H "Accept-Language: $LANGUAGE" \
-  -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+#curl \
+#  -H "Accept-Language: $LANGUAGE" \
+#  -H "User-Agent: $UA" \
+#  -o result.html \
+#  wttr.in/$CITY?format=4\&$UNIT
 
 #########################################
 ##                  @Mail Y0ur Linux   ##
@@ -32,6 +32,6 @@ mailContent_Type="text/html"
 mailTo="1904829268@qq.com"
 mailHTMLFile="/home/bin4xin/shares/share-files/bash/s-nail-bash-ps/google.html"
 
-echo "$serverIPaddr X $serverPlace X $serverDate X TOTAL $processCounts Processes" >> result.html
+echo "X IP地址：$serverIPaddr X 地址：$serverPlace X 当地时间：$serverDate X 共计： $processCounts进程" >> result.html
 
 
